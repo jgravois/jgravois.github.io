418e5453601651cd189339b496dcd2decaef7413
john gravois <jgravois@esri.com>
2016-04-07 18:07:59 -0700

    lookin better

--- maps/kc_jg/js/app.js
+++ maps/kc_jg/js/app.js
@@ -9,15 +9,38 @@ var map = L.map("map", {
   maxBounds: worldBounds
 }).setView([37.75, -122.23], 3);
 
-L.esri.basemapLayer("Streets", {
+L.esri.basemapLayer("Gray", {
   noWrap: true
 }).addTo(map);
 
+var done = "'France', 'Switzerland', 'Morocco', 'Australia', 'Spain', 'Japan', ";
+
+var soon = "'Australia', 'New Zealand', 'Belize', 'Costa Rica', 'India', 'Chile'";
+
 L.esri.featureLayer({
   url: 'http://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Countries_(Generalized)/FeatureServer/0',
-  where: "COUNTRY IN('France', 'Switzerland', 'Morocco', 'United States', 'Australia', 'Spain', 'Japan')",
+  where: "Country IN(" + done + soon + ")",
+  style: function (feature) {
+    if (soon.search(feature.properties.Country) > 0) {
+      return {
+        color: '#F2D653',
+        weight: 1.5
+      }
+    }
+    else {
+      return {
+        color: '#37424A',
+        weight: 1.5
+      }
+    }
+  }
+}).addTo(map);
+
+L.esri.featureLayer({
+  url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3',
+  where: "STATE_NAME IN('California', 'New Jersey', 'Hawaii', 'Utah', 'Washington', 'Oregon', 'Nevada', 'Louisiana', 'Florida', 'Georgia', 'Colorado')",
   style: {
-    fillOpacity: 0,
+    color: '#37424A',
     weight: 1.5
   }
 }).addTo(map);
